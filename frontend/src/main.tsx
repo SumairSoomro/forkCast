3863e96c4f2a802be9417ea229662ed9299cba84
Loick Marion <loick.marion@gmail.com>
2025-02-27 22:28:44 -0500

    forgot to add in previous commit #3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Add Router, Route, Routes
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
-import HomePage from './pages/homepage/Homepage.tsx'; // You may need to create this as a separate component
+import HomePage from './pages/homepage/Homepage.tsx'; 
 
 
 createRoot(document.getElementById('root')!).render(
