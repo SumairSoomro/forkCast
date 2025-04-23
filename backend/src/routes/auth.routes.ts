33087f1dfa9ee5fd14a492ce0163b90504bb9011
Sumair Soomro <143203396+SumairSoomro@users.noreply.github.com>
2025-04-23 14:50:49 -0400

    added logout implementation for backend

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { signup, login } from "../controllers/auth.controller";
+import { signup, login,getUser,logout } from "../controllers/auth.controller";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/user", getUser);
+router.post("/logout", logout);
 
 export default router;
